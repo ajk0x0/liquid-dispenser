5444e3af8a7c5037eaf61db0d609a985180f5e21
Abhijith <ajk0x00@gmail.com>
2022-06-28 16:59:01 +0530

    completed database

--- code.c.ino
+++ code.c.ino
@@ -13,17 +13,47 @@ void setup() {
   Serial.begin(9600);
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522
-  if (!db::checkInit()){
+  db::reset();
+  if (!db::checkInit())
     db::initialize();
-  }
+//  db::updateEntry(entry.address, entry.usage+1, Clock::getClock());
+//  db::resetEntryAddress();
+//  db::dumpData();
+//  db::resetEntryAddress();
+//  entry = db::nextEntry();
+  db::dumpData();
 }
  
 void loop() {
-  db::updateClock();
+  delay(500);
+  Clock::updateClock();
+  db::resetEntryAddress();
+  unsigned long currentTime = Clock::getClock();
   if(!rfid.PICC_IsNewCardPresent()) return;
   if (!rfid.PICC_ReadCardSerial()) return;
   String uid = getUID(rfid.uid.uidByte, rfid.uid.size);
-  print(uid);
+  print("has next: "); print(db::hasNextEntry());
+  while(db::hasNextEntry()){
+    db::Entry entry = db::nextEntry();
+    char rfid[9];
+    uid.toCharArray(rfid, 9);
+    if (!strcmp(rfid, entry.uid)){
+      if((currentTime - entry.lastUse) > 30000){
+        print("time update");
+        db::updateEntry(entry.address, 0, currentTime);
+      }else if (entry.usage > 2){
+        // reject code
+        print("Card rejected");print("\n");
+        return;
+      }else{
+        db::updateEntry(entry.address, entry.usage+1, currentTime);
+      }
+      // open outlet
+    }else{
+      print("rfid do not match : "); print(uid);
+    }
+  }
+  db::dumpData();
 }
 
 
